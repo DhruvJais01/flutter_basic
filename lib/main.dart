17df461f7785879e40f9f63c2e3d4c61ae580127
DhruvJais01 <dhruvjaiswal400@gmail.com>
2024-03-08 17:16:34 +0530

    Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -4,57 +4,109 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FirstPage() 
+      home: MyAppEx()
     );
   }
 }
+// to route we need statefull widget because it has context
+class MyAppEx extends StatefulWidget {
+  const MyAppEx({super.key});
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+  @override
+  State<MyAppEx> createState() => _MyAppExState();
+}
 
+class _MyAppExState extends State<MyAppEx> {
+  String buttonName = 'Click';
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Title'),
+          title: const Text('App Title'),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return const SecondPage();
-                  },
-                ),
-              );
-            },
-            child: const Text('Press'),
-          ),
+          child: currentIndex == 0
+              ? Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  color: Colors.deepPurpleAccent.shade400,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Color.fromARGB(232, 218, 16, 16),
+                          backgroundColor: Colors.orange[200],
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            buttonName = 'Clicked';
+                          });
+                        },
+                        child: Text(buttonName),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => NextPage(),
+                            ),
+                          );
+                        },
+                        child: const Text('Next Page'),
+                      ),
+                    ],
+                  ),
+                )
+              // : Image.network(
+              //     'https://www.tripsavvy.com/thmb/HOyyPeqcMSo_O_ZJ2eLosHEfz9E=/2121x1414/filters:fill(auto,1)/GettyImages-498928946-59cd1dd3af5d3a0011d3a87e.jpg'),
+              : Image.asset('images/sky.jpg'),
+          // uncomment assets in pubspec.yaml file, download package or flutter pub get (in bash)
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Setttings',
+              icon: Icon(Icons.settings),
+            )
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
         ),
       );
   }
 }
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // automaticallyImplyLeading: false,
-      ),
+      appBar: AppBar(),
     );
   }
 }
