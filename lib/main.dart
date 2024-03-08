657f74c9c787965c62d178538fac5b781ed1f841
DhruvJais01 <dhruvjaiswal400@gmail.com>
2024-03-08 15:20:51 +0530

    Images

--- lib/main.dart
+++ lib/main.dart
@@ -23,37 +23,42 @@ class _MyAppState extends State<MyApp> {
           title: const Text('App Title'),
         ),
         body: Center(
-          child: currentIndex==0? Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: Colors.deepPurpleAccent.shade400,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor:Color.fromARGB(232, 218, 16, 16),
-                    backgroundColor: Colors.orange[200],
+          child: currentIndex == 0
+              ? Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  color: Colors.deepPurpleAccent.shade400,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Color.fromARGB(232, 218, 16, 16),
+                          backgroundColor: Colors.orange[200],
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            buttonName = 'Clicked';
+                          });
+                        },
+                        child: Text(buttonName),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            buttonName = 'Clicked';
+                          });
+                        },
+                        child: Text(buttonName),
+                      ),
+                    ],
                   ),
-                  onPressed: () {
-                    setState(() {
-                      buttonName = 'Clicked';
-                    });
-                  },
-                  child: Text(buttonName),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      buttonName = 'Clicked';
-                    });
-                  },
-                  child: Text(buttonName),
-                ),
-              ],
-            ),
-          ):const SizedBox(),
+                )
+              // : Image.network(
+              //     'https://www.tripsavvy.com/thmb/HOyyPeqcMSo_O_ZJ2eLosHEfz9E=/2121x1414/filters:fill(auto,1)/GettyImages-498928946-59cd1dd3af5d3a0011d3a87e.jpg'),
+              : Image.asset('images/sky.jpg'),
+          // uncomment assets in pubspec.yaml file, download package or flutter pub get (in bash)
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
