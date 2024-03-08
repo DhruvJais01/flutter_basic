48aaa5d08d3bd2d50ab2f6cc81e15102f64eb1fa
DhruvJais01 <dhruvjaiswal400@gmail.com>
2024-03-08 17:48:03 +0530

    GestureDestector

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyAppEx()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyAppEx());
   }
 }
+
 // to route we need statefull widget because it has context
 class MyAppEx extends StatefulWidget {
   const MyAppEx({super.key});
@@ -32,71 +29,79 @@ class MyAppEx extends StatefulWidget {
 class _MyAppExState extends State<MyAppEx> {
   String buttonName = 'Click';
   int currentIndex = 0;
+  bool _isClicked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('App Title'),
-        ),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  color: Colors.deepPurpleAccent.shade400,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Color.fromARGB(232, 218, 16, 16),
-                          backgroundColor: Colors.orange[200],
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Clicked';
-                          });
-                        },
-                        child: Text(buttonName),
+      appBar: AppBar(
+        title: const Text('App Title'),
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.deepPurpleAccent.shade400,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Color.fromARGB(232, 218, 16, 16),
+                        backgroundColor: Colors.orange[200],
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) => NextPage(),
-                            ),
-                          );
-                        },
-                        child: const Text('Next Page'),
-                      ),
-                    ],
-                  ),
-                )
-              // : Image.network(
-              //     'https://www.tripsavvy.com/thmb/HOyyPeqcMSo_O_ZJ2eLosHEfz9E=/2121x1414/filters:fill(auto,1)/GettyImages-498928946-59cd1dd3af5d3a0011d3a87e.jpg'),
-              : Image.asset('images/sky.jpg'),
-          // uncomment assets in pubspec.yaml file, download package or flutter pub get (in bash)
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              label: 'Setttings',
-              icon: Icon(Icons.settings),
-            )
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      );
+                      onPressed: () {
+                        setState(() {
+                          buttonName = 'Clicked';
+                        });
+                      },
+                      child: Text(buttonName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked
+                    ? Image.asset('images/sky.jpg')
+                    : Image.network(
+                        'https://www.tripsavvy.com/thmb/HOyyPeqcMSo_O_ZJ2eLosHEfz9E=/2121x1414/filters:fill(auto,1)/GettyImages-498928946-59cd1dd3af5d3a0011d3a87e.jpg'),
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Setttings',
+            icon: Icon(Icons.settings),
+          )
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
 
